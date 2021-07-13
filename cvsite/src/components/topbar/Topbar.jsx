2d9fb2f3d83bec08723da5acebd6b36dbfdee1bf
Jose <jnavacar1998@gmail.com>
2021-07-13 16:38:01 +0200

    Top left side prototyped

--- cvsite/src/components/topbar/Topbar.jsx
+++ cvsite/src/components/topbar/Topbar.jsx
@@ -1,11 +1,22 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">asdsads</a>
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+12123213</span>
+
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>yeet@genius.com</span>
+                        
+                    </div>
                 </div>
                 <div className="right">
                 </div>
