698f21c79096552691cce4974855919e8cabab8c
Jose <jnavacar1998@gmail.com>
2021-07-14 15:19:47 +0200

    Added new animation
    Text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(
+            textRef.current, { 
+                showCursor: true, 
+                backDelay: 1500,
+                backSpeed: 60,
+                strings: ['Student', 'Engineer', 'Lover' ] 
+        });
+
+    }, [])
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/portrait.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Jose Navarrete Carbonell</h1>
+                    <h3>Software <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/arrow_down.png" alt=""></img>
+                </a>
+            </div>
         </div>
     )
 }
