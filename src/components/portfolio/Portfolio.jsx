6e0202f9a49d92c54881c00f30e2fb8c66caaa9b
Jose <jnavacar1998@gmail.com>
2021-07-15 05:07:26 +0200

    Portfolio WIP
    Finished skeleton of portfolio. Added Link functionality (not guided by tutorial). Creating and adjusting image sizes.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,22 +1,81 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
+import { featuredPortfolio, webappPortfolio, gamedevPortfolio, csharpPortfolio, cPortfolio } from "../../data";
 
 export default function Portfolio() {
+
+    const [selected, setSelected] = useState("featured");
+    const [data, setData] = useState([]);
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "game",
+            title: "Game Dev"
+        },
+        {
+            id: "C#",
+            title: "C#"
+        },
+        {
+            id: "C",
+            title: "C"
+        }
+    ];
+
+    useEffect(() => {
+
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webappPortfolio);
+                break;
+            case "game":
+                setData(gamedevPortfolio);
+                break;
+            case "C#":
+                setData(csharpPortfolio);
+                break;
+            case "C":
+                setData(cPortfolio);
+                break;
+            default:
+                setData(featuredPortfolio);
+        }
+    }, [selected])
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
-                <li>Web App</li>
-                <li>Game Dev</li>
-                <li>C#</li>
-                <li>C</li>
+                {list.map((item) => (
+                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+                ))}
             </ul>
 
             <div className="container">
-                <div className="item">
-                    <img src="assets/ticketwebsite.png" alt="" />
-                    <h3>Ticket Website</h3>
-                </div>
+                {data.map((d) => (
+                    <div className="item">
+                        <a href={d.link}>
+                            <img 
+                                src= {d.img}
+                                alt=""
+                            />
+                            <h3 href={d.link}>{d.title}</h3>
+                        </a>
+                    </div>
+                ))}
+                
             </div>
         </div>
     )
---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Ticket Website",
+        img: "assets/ticketwebsite.png",
+        link: "https://github.com/jnavacar/TicketVendingWebsite-ReactJS-SpringBoot"
+    },
+    {
+        id: 2,
+        title: "Portfolio Website",
+        img: "assets/portfolioimg.png",
+        link: "https://github.com/jnavacar/jnavacar-cvsite"
+    },
+    {
+        id: 3,
+        title: "NightOfTheNerds",
+        img: "assets/unitylogo.png",
+        link: "https://github.com/jnavacar/NightOfTheNerdsEscapeRoom-UnityProject"
+    }
+];
+
+export const webappPortfolio = [
+    {
+        id: 1,
+        title: "Ticket Website",
+        img: "assets/ticketwebsite.png",
+        link: "https://github.com/jnavacar/TicketVendingWebsite-ReactJS-SpringBoot"
+    },
+    {
+        id: 2,
+        title: "Portfolio Website",
+        img: "assets/portfolioimg.png",
+        link: "https://github.com/jnavacar/jnavacar-cvsite"
+    }
+];
+
+export const gamedevPortfolio = [
+    {
+        id: 1,
+        title: "CyberJump",
+        img: "assets/unitylogo.png",
+        link: "https://github.com/jnavacar/CyberJump-UnityProject"
+    },
+    {
+        id: 2,
+        title: "Doffend",
+        img: "assets/unitylogo.png",
+        link: "https://github.com/jnavacar/Doffend-UnityProject"
+    },
+    {
+        id: 3,
+        title: "NightOfTheNerds",
+        img: "assets/unitylogo.png",
+        link: "https://github.com/jnavacar/NightOfTheNerdsEscapeRoom-UnityProject"
+    }
+];
+
+export const csharpPortfolio = [
+    
+    {
+        id: 1,
+        title: "Complaint App",
+        img: "assets/csharplogo.png",
+        link: "https://github.com/jnavacar/ComplaintApp-WinFormApp"
+    }
+    
+];
+
+export const cPortfolio = [
+    {
+
+    }
+];
